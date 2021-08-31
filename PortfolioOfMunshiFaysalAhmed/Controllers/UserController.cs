12e69c7fd59aac8ae62a4f678937282dd5a08899
munshi-faysal <18203036@iubat.edu>
2021-09-01 02:20:42 +0600

    first commit

---
+++ PortfolioOfMunshiFaysalAhmed/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using PortfolioOfMunshiFaysalAhmed.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioOfMunshiFaysalAhmed.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(user_msg obj)
+
+        {
+            if (ModelState.IsValid)
+            {
+                portfolioEntities db = new portfolioEntities();
+                db.user_msg.Add(obj);
+                db.SaveChanges();
+            }
+            ViewBag.Message = "Message has been successfullly submitted!!!";
+            return View(obj);
+        }
+    }
+}
